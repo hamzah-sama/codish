4fd7457f5665f3a74e096ff157d23a7a611d2bb2
Hamzah <hamzah.sama@email.com>
2026-04-18 13:53:19 +0700

    refactor

--- src/app/projects/[projectId]/layout.tsx
+++ src/app/projects/[projectId]/layout.tsx
@@ -3,17 +3,15 @@ import { Id } from "../../../../convex/_generated/dataModel";
 
 interface Props {
   children: React.ReactNode;
-  params: Promise<{ projectId: Id<"projects"> }>;
+  params: Promise<{ projectId: string }>;
 }
 
 const Layout = async ({ children, params }: Props) => {
-  const { projectId } = await params;
+  const projectId = (await params).projectId as Id<"projects">;
   return (
     <div className="w-full h-screen flex flex-col">
       <ProjectNavbar projectId={projectId} />
-      <div className="flex flex-1 overflow-hidden bg-sidebar">
-        {children}
-      </div>
+      <div className="flex flex-1 overflow-hidden bg-sidebar">{children}</div>
     </div>
   );
 };
